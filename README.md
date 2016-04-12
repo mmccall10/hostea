a8675849c0cd6c7a174ae6b0c5ce0f82ec46537e
Mike McCall <mike.mccall@hdmz.com>
2016-04-12 16:02:27 -0700

    add user input

--- README.md
+++ README.md
@@ -2,8 +2,26 @@
 
 Command line tool to quickly configure new apache vhost
 
-The current build is intended to be used locally for quickly configuring apache vhost using scotch-box and vagrant.
+The current build is intended to be used to quickly configure apache vhost using scotch-box and vagrant for local development. Yet to be tested elsewhere.
+
+
+<code>[sudo] npm install -g hostea</code>
+
+Dealing with apache conf it's likely need to run it as sudo.
+
+Usage:
 
 
 
+<code>
+sudo hostea<br>
+Site URL: example.dev<br>
+Web Root (/var/www/):<br>
+Site configuration has been created!<br>
+Do you want to restart apache now (service apache2 restart)? [y,n] y<br>
+ * Restarting web server apache2
+   ...done.
+Don't forget to set up your local host file!<br>
+sudo echo "192.168.33.10    example.dev" >> /etc/hosts<br>
+Happy Coding
 
--- index.js
+++ index.js
@@ -3,91 +3,163 @@ var fs = require('fs');
 var path = require('path');
 var readline = require('readline');
 var exec = require('child_process').exec;
+var chalk = require('chalk');
+var ip = require('ip');
+
+var os = require('os');
+
 
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    terminal: false
 });
 
-//TODO - gather user input and add config option
-var sitesAvailableDir = '/etc/apache2/sites-available/';
-var sitesEnabledDir = '/etc/apache2/sites-enabled/';
-var documentRoot = 'var/www/';
+//TODO - save config
 
-//TODO - ask if no url provided
-if (process.argv.length <= 2) {
-
-    console.log("Usage: node scotch-host.js site-url");
-    process.exit(-1);
-
-} else {
-
-    var siteConfName = process.argv[2];
-
-    fs.exists(sitesAvailableDir + siteConfName + '.conf', function (exists) {
-        if (exists) {
-            console.log('Apache configuration ' + sitesAvailableDir.siteConfName + ' already exists');
-            process.exit(-1);
-        }
-        makeApacheConfigFile();
-    });
-
+var config = {
+    sitesAvailableDir: '/etc/apache2/sites-available/',
+    sitesEnabledDir: '/etc/apache2/sites-enabled/',
+    documentRoot: '/var/www/',
+    siteUrl: '',
+    ip: ''
 }
 
+
+var askForUrl = function(){
+
+    rl.question(chalk.blue('Site URL: '), function (answer) {
+
+        if(answer.length > 0) {
+
+            config.siteUrl = answer;
+
+        }else if(answer.length == 0){
+
+            console.log(chalk.red("Must enter a site url."));
+            askForUrl();
+            return;
+
+        }
+        askForWebRoot();
+
+    });
+}
+
+var askForWebRoot = function(){
+
+    rl.question(chalk.blue('Web Root (/var/www/): '), function (answer) {
+
+        if(answer.length > 0) {
+            config.documentRoot = answer;
+        }
+
+        makeApacheConfigFile();
+
+    });
+}
+
+
+
+var getIpForHostFile = function(){
+    var ifaces = os.networkInterfaces();
+
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+            if(ifname == 'eth1'){
+                config.ip = iface.address;
+            }
+
+            ++alias;
+        });
+
+
+    });
+}
+
+var finish = function(){
+    console.log(chalk.bgGreen('Don\'t forget to set up your local host file!'));
+
+    if(config.ip.length > 0){
+        console.log(chalk.bgWhite('sudo echo "'+config.ip+' '+config.siteUrl+'" >> /etc/hosts'));
+
+    }
+
+    console.log(chalk.green('Happy Coding'));
+
+
+    process.exit(-1);
+}
+
+//setup config
+
 var makeApacheConfigFile = function(){
+
 
     var vHostFile = ''+
         '<VirtualHost *:80>'+
         '\n   ServerAdmin webmaster@localhost'+
-        '\n   ServerName '+siteConfName+
-        '\n   ServerAlias '+siteConfName+
-        '\n   DocumentRoot /'+documentRoot+siteConfName+
+        '\n   ServerName '+config.siteUrl+
+        '\n   ServerAlias '+config.siteUrl+
+        '\n   DocumentRoot /'+config.documentRoot+config.siteUrl+
         '\n   ErrorLog ${APACHE_LOG_DIR}/error.log'+
         '\n   CustomLog ${APACHE_LOG_DIR}/access.log combined'+
         '\n</VirtualHost>';
 
 
-    fs.writeFileSync(sitesAvailableDir+siteConfName+'.conf', vHostFile);
+    fs.writeFileSync(config.sitesAvailableDir+config.siteUrl+'.conf', vHostFile);
 
-    fs.symlink(sitesAvailableDir+siteConfName+'.conf', sitesEnabledDir+siteConfName+'.conf', function() {
+    fs.symlink(config.sitesAvailableDir+config.siteUrl+'.conf', config.sitesEnabledDir+config.siteUrl+'.conf', function() {
 
         //TODO - provide host file config
-        console.log('Site configuration has been created! ');
+        console.log(chalk.green('Site configuration has been created! '));
 
-        rl.question('Do you want to restart apache now (service apache2 restart)? [y,n] ', function (answer) {
+        rl.question(chalk.blue('Do you want to restart apache now (service apache2 restart)? [y,n] '), function (answer) {
 
             if(answer =='y' || answer == 'Y' || answer == 'Yes' || answer == 'yes'){
 
                 exec('service apache2 restart', function(err, stdout, stderr){
                     if (err) {
 
-                        console.error(err);
-
+                        console.error(chalk.red(err));
                         return;
 
                     }
 
-                    console.log(stdout);
+                    if(stderr){
+                        console.log(chalk.yellow(stderr));
+                    }
+
+                    console.log(chalk.green(stdout));
+
+                    rl.close();
+                    finish();
 
                 });
 
             }else{
-
-                console.log('Ok, Happy Coding!');
+                rl.close();
+                finish();
 
             }
 
-            rl.close();
+
         });
 
     });
 
 
-
-
 }
 
+askForUrl();
+getIpForHostFile();
 
 
 
